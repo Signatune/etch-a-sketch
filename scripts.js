dd7a8475be3dace516ad9133e406b5bea1c7b1fa
Mike <michaelambrosino93@gmail.com>
2023-05-25 13:01:55 -0400

    basic working sketch board

---
+++ scripts.js
@@ -0,0 +1,18 @@
+let sketchBoard = document.querySelector(".sketch-board");
+
+let gridSize = 25;
+
+for (let row = 0; row < gridSize; row++) {
+  let row = document.createElement("div");
+  row.classList.add("board-row");
+  sketchBoard.appendChild(row);
+  for (let cell = 0; cell < gridSize; cell++) {
+    let cell = document.createElement("div");
+    cell.classList.add("cell");
+    row.appendChild(cell);
+
+    cell.addEventListener("click", (event) => {
+      cell.classList.add("filled");
+    });
+  }
+}
